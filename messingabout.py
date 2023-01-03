1d469c96e612d3688e5f7fd2921f168f242c3119
PhilT415 <58818395+PhilT415@users.noreply.github.com>
2023-01-03 13:15:29 -0500

    more commits

--- messingabout.py
+++ messingabout.py
@@ -1,1 +1,12 @@
-print ("hello")
+
+
+
+def concatenate(**words):
+    result = 0
+    for arg in words.values():
+        result += arg
+    return result
+
+print(concatenate(a=1, b=2, c=5, d=5, e=125))
+
+
